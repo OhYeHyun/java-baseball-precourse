1fdda768743477ddacf87467f56293e4ba0c94d9
OhYeHyun <ohyhohyl@naver.com>
2024-11-30 00:07:55 +0900

    feat(Application): 게임 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import baseball.controller.BaseBallController;
+import baseball.domain.GameResults;
+import baseball.initializer.GameInitializer;
+import baseball.service.NumberService;
+import baseball.service.ResultService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameResults gameResults = GameResults.getInstance();
+        GameInitializer initializer = new GameInitializer(gameResults);
+        NumberService numberService = new NumberService();
+        ResultService resultService = new ResultService(gameResults);
+
+        BaseBallController controller = new BaseBallController(initializer, numberService, resultService);
+        controller.run();
     }
 }
