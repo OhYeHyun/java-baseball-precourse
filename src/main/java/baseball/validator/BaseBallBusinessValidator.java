6a172218a4e0c7c08ab3e774c4c329ccb9d25bd1
OhYeHyun <ohyhohyl@naver.com>
2024-11-30 00:02:45 +0900

    fix(BusinessValidator): 입력값을 파서한 값으로 수정

--- src/main/java/baseball/validator/BaseBallBusinessValidator.java
+++ src/main/java/baseball/validator/BaseBallBusinessValidator.java
@@ -1,45 +1,43 @@
 package baseball.validator;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.List;
 
 public class BaseBallBusinessValidator {
 
-    public static void validateUserNumber(String userNumber) {
+    public static void validateUserNumber(List<Integer> userNumber) {
         userNumberLength(userNumber);
         userNumberRange(userNumber);
         userNumberUnique(userNumber);
     }
 
-    public static void validateContinueNumber(String continueNumber) {
+    public static void validateContinueNumber(int continueNumber) {
         continueNumberExactly(continueNumber);
     }
 
-    private static void userNumberLength(String userNumber) {
-        if (userNumber.length() != 3) {
+    private static void userNumberLength(List<Integer> userNumber) {
+        if (userNumber.size() != 3) {
             throw new IllegalArgumentException("[ERROR] 3자리 숫자로 입력해 주세요.");
         }
     }
 
-    private static void userNumberRange(String userNumber) {
-        Arrays.stream(userNumber.split("")).forEach(num -> {
-            int number = Integer.parseInt(num);
-            if (number < 1 || number > 9) {
+    private static void userNumberRange(List<Integer> userNumber) {
+        userNumber.forEach(num -> {
+            if (num < 1 || num > 9) {
                 throw new IllegalArgumentException("[ERROR] 1 ~ 9 사이의 숫자로 입력해 주세요.");
             }
         });
     }
 
-    private static void userNumberUnique(String userNumber) {
-        int size = Arrays.stream(userNumber.split("")).collect(Collectors.toSet()).size();
+    private static void userNumberUnique(List<Integer> userNumber) {
+        int size = new HashSet<>(userNumber).size();
         if (size != 3) {
             throw new IllegalArgumentException("[ERROR] 서로 다른 숫자로 입력해 주세요.");
         }
     }
 
-    private static void continueNumberExactly(String continueNumber) {
-        int number = Integer.parseInt(continueNumber);
-        if (number != 1 && number != 2) {
+    private static void continueNumberExactly(int continueNumber) {
+        if (continueNumber != 1 && continueNumber != 2) {
             throw new IllegalArgumentException("[ERROR] 1 또는 2로 입력해 주세요");
         }
     }
